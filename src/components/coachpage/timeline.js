83f676293b0a345878ce8bf3edceeab74fa339ab
anthow <anthonyenglebert@gmail.com>
2022-05-15 13:37:07 +0200

    ligne du temps smartphone

--- src/components/coachpage/timeline.js
+++ src/components/coachpage/timeline.js
@@ -16,35 +16,41 @@ const Timeline = () => {
     }
   `);
   return (
-    <section className="py-10 w-10/12 lg:w-8/12 m-auto text-center text-4xl font-black">
-      <div class="container  mx-auto w-full h-full">
-        <div class="relative wrap overflow-hidden p-10 h-full">
-          <div
-            class="border-2-2 absolute timeline border-opacity-20 border-gray-700 h-full border"
-            styles="left: 50%"
-          ></div>
-
-          {/* left timeline */}
-          <div class="mb-8 flex   justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1  w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl  w-5/12 px-6 py-4">
+    <div className="flex flex-col items-center mt-5">
+          <div class="container  mx-auto w-full h-full">
+            <div class="relative  text-third-color wrap overflow-hidden p-10 h-full">
               <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+                class="border-2-2 absolute  timeline border-opacity-20 border-gray-700 h-full border"
+                styles="left: 50%"
+              ></div>
+
+{/* left timeline */}
+<div class="mb-8 flex flex-col md:flex-row justify-between md:flex-row-reverse items-center w-full left-timeline">
+                <div class="order-1 w-5/12"></div>
+                <div class="z-20 flex mb-10 md:mb-0 items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto text-white font-semibold text-lg">1</h1>
+                </div>
+                <div class="order-1 color shadow-xl bg-white rounded-lg z-20 text-white w-12/12 md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium z-20 text-primary-color text-opacity-100  "
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteUnLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* right timeline */}
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color  shadow-xl w-5/12 px-6 py-4">
-              <div
+  
+                </div>
+              </div>
+
+              {/* right timeline */}
+              <div class="my-8  flex flex-col md:flex-row justify-between  items-center w-full md:right-timeline">
+                <div class="order-1  w-12/12 md:w-5/12"></div>
+                <div class="z-20 flex mb-5 md:mb-0  items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto font-semibold text-lg text-white">2</h1>
+                </div>
+                <div class="order-1 text-primary-color bg-white  z-20 rounded-lg shadow-xl md:w-5/12 px-6 py-4">
+                <div
                 className="text-sm font-medium   text-primary-color text-opacity-100"
                 dangerouslySetInnerHTML={{
                   __html:
@@ -52,87 +58,104 @@ const Timeline = () => {
                       .texteDeuxLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* left timeline */}
-          <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl w-5/12 px-6 py-4">
-              <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+                </div>
+              </div>
+{/* left timeline */}
+<div class="mb-8 flex flex-col md:flex-row justify-between md:flex-row-reverse items-center w-full left-timeline">
+                <div class="order-1 w-5/12"></div>
+                <div class="z-20 flex mb-10 md:mb-0 items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto text-white font-semibold text-lg">1</h1>
+                </div>
+                <div class="order-1 color shadow-xl bg-white rounded-lg z-20 text-white w-12/12 md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium z-20 text-primary-color text-opacity-100  "
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteTroisLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* right timeline */}
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl w-5/12 px-6 py-4">
-              <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+  
+                </div>
+              </div>
+
+              {/* right timeline */}
+              <div class="my-8  flex flex-col md:flex-row justify-between  items-center w-full md:right-timeline">
+                <div class="order-1  w-12/12 md:w-5/12"></div>
+                <div class="z-20 flex mb-5 md:mb-0  items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
+                </div>
+                <div class="order-1 text-primary-color bg-white  z-20 rounded-lg shadow-xl md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium   text-primary-color text-opacity-100"
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteQuatreLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* left timeline */}
-          <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl w-5/12 px-6 py-4">
-              <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+                </div>
+              </div>
+
+              {/* left timeline */}
+<div class="mb-8 flex flex-col md:flex-row justify-between md:flex-row-reverse items-center w-full left-timeline">
+                <div class="order-1 w-5/12"></div>
+                <div class="z-20 flex mb-10 md:mb-0 items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
+                </div>
+                <div class="order-1 color shadow-xl bg-white rounded-lg z-20 text-white w-12/12 md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium z-20 text-primary-color text-opacity-100  "
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteCinqLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* right timeline */}
-          <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl w-5/12 px-6 py-4">
-              <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+  
+                </div>
+              </div>
+
+              {/* right timeline */}
+              <div class="my-8  flex flex-col md:flex-row justify-between  items-center w-full md:right-timeline">
+                <div class="order-1  w-12/12 md:w-5/12"></div>
+                <div class="z-20 flex mb-5 md:mb-0  items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto font-semibold text-lg text-white">5</h1>
+                </div>
+                <div class="order-1 text-primary-color bg-white  z-20 rounded-lg shadow-xl md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium   text-primary-color text-opacity-100"
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteSixLigneDuTemps,
                 }}
               />
-            </div>
-          </div>
-          {/* left timeline */}
-          <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-primary-color shadow-xl w-6 h-6 rounded-full"></div>
-            <div class="order-1 color shadow-xl w-5/12 px-6 py-4">
-              <div
-                className="text-sm font-medium  text-primary-color text-opacity-100"
+                </div>
+              </div>
+              
+        {/* left timeline */}
+        <div class="mb-8 flex flex-col md:flex-row justify-between md:flex-row-reverse items-center w-full left-timeline">
+                <div class="order-1 w-5/12"></div>
+                <div class="z-20 flex mb-10 md:mb-0 items-center order-1 bg-primary-color shadow-xl w-8 h-8 rounded-full">
+                  <h1 class="mx-auto text-white font-semibold text-lg">6</h1>
+                </div>
+                <div class="order-1 color shadow-xl bg-white rounded-lg z-20 text-white w-12/12 md:w-5/12 px-6 py-4">
+                <div
+                className="text-sm font-medium z-20 text-primary-color text-opacity-100  "
                 dangerouslySetInnerHTML={{
                   __html:
                     data.datoCmsPageAccompagnementIndividuel
                       .texteSeptLigneDuTemps,
                 }}
               />
+  
+                </div>
+              </div>
+            
             </div>
           </div>
-        </div>
-      </div>
-    </section>
-  );
+        </div>)
 };
 
 export default Timeline;
