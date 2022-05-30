79bf0d0db7e4c61c63f06857b1dd00dfd1647ca0
anthow <anthonyenglebert@gmail.com>
2022-05-30 15:23:30 +0200

    ajout bouton d'appel

--- src/pages/coaching.js
+++ src/pages/coaching.js
@@ -8,6 +8,7 @@ import Timeline from "../components/coachpage/timeline";
 import Header from "../components/coachpage/header";
 import Offre from "../components/coachpage/offre";
 import Groupe from "../components/footer/groupe";
+import Appel from "../components/coachpage/appel";
 
 const CoachingPage = () => {
   return (
@@ -17,7 +18,7 @@ const CoachingPage = () => {
       <Header />
           <Timeline />
      <Offre />
-    
+    <Appel />
         <Groupe />
     </Layout>
     </>
