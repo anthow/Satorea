1a244ab0abfa7abd1062c33165154a069591b407
anthow <anthonyenglebert@gmail.com>
2022-08-24 16:03:26 +0200

    font

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,16 +14,15 @@ module.exports = {
     `gatsby-plugin-minify`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-sitemap`,
-    'gatsby-plugin-robots-txt',
-    
+    "gatsby-plugin-robots-txt",
 
     {
-      resolve: 'gatsby-plugin-html-attributes',
+      resolve: "gatsby-plugin-html-attributes",
       options: {
-        lang: 'fr'
-      }
+        lang: "fr",
+      },
     },
-        {
+    {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
@@ -40,13 +39,21 @@ module.exports = {
       __key: "images",
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: `gatsby-plugin-webfonts`,
       options: {
-        fonts: [
-          `Montserrat`,
-        ],
-        display: 'swap'
-      }
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400", "500", "600", "800"],
+              useMinify: true,
+              usePreload: true,
+              fontDisplay: "swap",
+              strategy: "selfHosted", // 'base64' || 'cdn'
+            },
+          ],
+        },
+      },
     },
 
     {
@@ -55,28 +62,26 @@ module.exports = {
         // You can find your read-only API token under the Settings > API tokens
         // section of your administrative area. Make sure to grant both CDA and CMA permissions.
         apiToken: `9c1d51e681903b32725f60db78a3cb`,
-  
+
         // The project environment to read from. Defaults to the primary environment:
         environment: `main`,
-  
+
         // If you are working on development/staging environment, you might want to
         // preview the latest version of records instead of the published one:
         previewMode: false,
-  
+
         // Disable automatic reloading of content when some change occurs on DatoCMS:
         disableLiveReload: false,
-  
+
         // Custom API base URL (most don't need this)
         // apiUrl: 'https://site-api.datocms.com',
-  
+
         // Setup locale fallbacks
         // In this example, if some field value is missing in Italian, fall back to English
         localeFallbacks: {
-          it: ['en'],
+          it: ["en"],
         },
-  
       },
     },
-    
   ],
 };
